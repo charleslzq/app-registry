8893924d05abb6d5318a8b8b945de55e2766999b
Eric Charles <charleslzq@gmail.com>
2017-05-27 17:13:59 +0800

    use tagged logger instead

--- app/controllers/app_registry_controller.rb
+++ app/controllers/app_registry_controller.rb
@@ -8,6 +8,9 @@ class AppRegistryController < ApplicationApiController
     info = InstanceInfo.new
     info.assign_attributes instance_params(params)
     info.last_heart_beat = Time.now
+    logger.tagged("#{app_id}", "#{params[:instance_id]}") {
+      logger.info 'Registering'
+    }
     registry[app_id][info.instance_id] = info
 
     render json: {
@@ -16,6 +19,9 @@ class AppRegistryController < ApplicationApiController
   end
 
   def de_register
+    logger.tagged("#{params[:app_id]}", "#{params[:instance_id]}") {
+      logger.info 'De-registering'
+    }
     registry[params[:app_id]].delete(params[:instance_id]) if registry.has_key? params[:app_id]
 
     render json: {
@@ -24,7 +30,12 @@ class AppRegistryController < ApplicationApiController
   end
 
   def listen_heart_beat
-    registry[params[:app_id]][params[:instance_id]].last_heart_beat = Time.now if registry.has_key?(params[:app_id]) and registry[params[:app_id]].has_key?(params[:instance_id])
+    app_id = params[:app_id]
+    instance_id = params[:instance_id]
+    logger.tagged("#{app_id}", "#{instance_id}") {
+      logger.info 'Heart-Beat'
+    }
+    registry[app_id][instance_id].last_heart_beat = Time.now if registry.has_key?(app_id) and registry[app_id].has_key?(instance_id)
 
     render json: {
         :success => true
