f9462d807850672cc181c86882ff01b8ba9dc3a7
Eric Charles <charleslzq@gmail.com>
2017-05-27 17:35:34 +0800

    move some logic out of controller

--- app/controllers/app_registry_controller.rb
+++ app/controllers/app_registry_controller.rb
@@ -1,17 +1,14 @@
 class AppRegistryController < ApplicationApiController
-  class_attribute :registry
-  self.registry = {}
 
   def register
     app_id = params[:app_id]
-    init_if_necessary app_id
     info = InstanceInfo.new
     info.assign_attributes instance_params(params)
     info.last_heart_beat = Time.now
     logger.tagged("#{app_id}", "#{params[:instance_id]}") {
       logger.info 'Registering'
     }
-    registry[app_id][info.instance_id] = info
+    InstanceInfo.register app_id, info
 
     render json: {
         :success => true
@@ -22,7 +19,7 @@ class AppRegistryController < ApplicationApiController
     logger.tagged("#{params[:app_id]}", "#{params[:instance_id]}") {
       logger.info 'De-registering'
     }
-    registry[params[:app_id]].delete(params[:instance_id]) if registry.has_key? params[:app_id]
+    InstanceInfo.de_register params[:app_id], params[:instance_id]
 
     render json: {
         :success => true
@@ -35,7 +32,8 @@ class AppRegistryController < ApplicationApiController
     logger.tagged("#{app_id}", "#{instance_id}") {
       logger.info 'Heart-Beat'
     }
-    registry[app_id][instance_id].last_heart_beat = Time.now if registry.has_key?(app_id) and registry[app_id].has_key?(instance_id)
+    instance = InstanceInfo.find_by_instance_id app_id, instance_id
+    instance.last_heart_beat = Time.now unless instance.nil?
 
     render json: {
         :success => true
@@ -43,30 +41,18 @@ class AppRegistryController < ApplicationApiController
   end
 
   def find_all
-    render json: registry
+    render json: InstanceInfo.find_all
   end
 
   def find_by_app_id
-    if registry.has_key?(params[:app_id])
-      render json: registry[params[:app_id]]
-    else
-      render json: {}
-    end
+    render json: InstanceInfo.find_by_app_id(params[:app_id])
   end
 
   def find_by_instance_id
-    if registry.has_key?(params[:app_id]) and registry[params[:app_id]].has_key?(params[:instance_id])
-      render json: registry[params[:app_id]][params[:instance_id]]
-    else
-      render json: {}
-    end
+    render json: InstanceInfo.find_by_instance_id(params[:app_id], params[:instance_id])
   end
 
   private
-  def init_if_necessary(app_id)
-    registry[app_id] = {} unless registry.has_key? app_id
-  end
-
   def instance_params(params)
     params.permit(:instance_id, :host_name, :host_ip, :host_port, :app_base, :app_id, meta_data: params[:meta_data].keys)
   end
--- app/data/instance_info.rb
+++ app/data/instance_info.rb
@@ -1,4 +1,42 @@
 class InstanceInfo
   include ActiveRecord::AttributeAssignment
+  class_attribute :registry
+  self.registry = {}
+
   attr_accessor :app_id, :instance_id, :host_name, :host_ip, :host_port, :app_base, :meta_data, :last_heart_beat
+
+  def self.register(app_id, info)
+    init_if_necessary app_id
+    registry[app_id][info.instance_id] = info
+  end
+
+  def self.de_register(app_id, instance_id)
+    registry[app_id].delete(instance_id) if registry.has_key? app_id
+  end
+
+  def self.find_all
+    registry
+  end
+
+  def self.find_by_app_id(app_id)
+    if registry.has_key?(app_id)
+      registry[app_id]
+    else
+      {}
+    end
+  end
+
+  def self.find_by_instance_id(app_id, instance_id)
+    if registry.has_key?(app_id) and registry[app_id].has_key?(instance_id)
+      registry[app_id][instance_id]
+    else
+      nil
+    end
+  end
+
+  private
+  def self.init_if_necessary(app_id)
+    registry[app_id] = {} unless registry.has_key? app_id
+  end
+
 end
